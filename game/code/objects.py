5b3e6030b7d5864057a1f3c7fbc4905f8925a084
Michael <MichaelGKnight87@gmail.com>
2020-03-29 00:37:38 +1000

    Main Div mousemove event now has simple path-finding. * Calculates shortest unblocked path from player to nearby walls and colours accordingly. ** Players can no longer see maze through walls as wall must be reachable withing 10 unobstructed steps. ** previously revealed tiles out of range of later updates remain the final colour applied (Dimmest) allowing for Fog-of-War style exploration

--- game/code/objects.py
+++ game/code/objects.py
@@ -7,3 +7,9 @@ class Start():
     def __init__(self, x,y):
         self.x = x
         self.y = y
+
+class Actor():
+    def __init__(self, x,y, behaviour = {}):
+        self.x = x
+        self.y = y
+        self.behaviour = behaviour
--- static/js/game.js
+++ static/js/game.js
@@ -1,54 +1,113 @@
 
-var game_state = 'idle';
+let game_state = 'idle';
+let wall_positions = [];
+let wall_array;
+let mouse_position = [0,0];
 
-window.onload = function level_init(){
-
-};
+window.onload = function level_init(){ };
 
 function game_start(){
+
+    document.getElementsByClassName('start')[0].style.opacity = 0;
     console.log('Game start');
     game_state = 'play';
+    wall_array = Array.from(document.getElementsByClassName('wall'));
+    wall_array.forEach(wall => {wall_positions.push([Math.round(wall.offsetLeft/50), Math.round(wall.offsetTop/50)])});
+
 }
 
 function background_flash(colour){
-    var background_pane = document.getElementsByClassName('gui_main_pane')[0];
-    background_pane.style.backgroundColor = colour
+    document.getElementsByClassName('gui_main_pane')[0].style.backgroundColor = colour;
 }
 
-function main_pane_restore_background(){
-   background_flash('Black')
-}
+function main_pane_restore_background(){ background_flash('Black') }
 
 function hit(){
-
-    if (game_state == 'play'){
+    if (game_state === 'play'){
         console.log('Game over');
         game_state = 'idle';
+        background_flash('Red');
+        Array.from(document.getElementsByClassName('wall')).forEach(wall => wall.style.opacity = 0);
+        document.getElementsByClassName('start')[0].style.opacity = 1;
+    }
+}
 
-        var wall_list = document.getElementsByClassName('wall');
-        background_flash('Red')
-        for(i=0; i<wall_list.length; i++){
-             wall_list[i].style.opacity = 0;
+
+
+function arrays_contains_pos(tested_array, target){
+    for(index=0;index<tested_array.length;index++){
+        if(tested_array[index][0] === target[0] && tested_array[index][1] === target[1]){
+            return true;
         }
     }
+    return false;
+}
 
-
+function array_index_pos(tested_array, target){
+    for(index=0;index<tested_array.length;index++){
+        if(tested_array[index][0] === target[0] && tested_array[index][1] === target[1]){
+            return index;
+        }
+    }
+    return false;
 }
 
 function move(e){
-    var mouse_x = Math.round(e.clientX/50) ;
-    var mouse_y = Math.round(e.clientY/50) ;
+    if (game_state === 'play') {
+
+        let mouse_x = Math.round(e.clientX / 50);
+        let mouse_y = Math.round(e.clientY / 50);
+
+        if(mouse_position[0] === mouse_x && mouse_position[1] === mouse_y){return;}
+        mouse_position = [mouse_x, mouse_y];
 
 
+        let opacity_value;
 
-    if (game_state == 'play'){
-        var wall_list = document.getElementsByClassName('wall');
-        for(i=0; i<wall_list.length; i++){
 
-            object_left = Math.round((wall_list[i].offsetLeft + wall_list[i].clientWidth/2)/50);
-            object_top = Math.round((wall_list[i].offsetTop + wall_list[i].clientHeight/2)/50);
-            wall_list[i].style.opacity = 1- (Math.abs(object_left-mouse_x)+Math.abs(object_top-mouse_y))/15;
+        let current_position;
+        let next_position;
+
+        let checked_positions = [];
+        let to_be_checked_positions = [mouse_position];
+        let to_be_checked_next = [];
+
+        let check1 = false;
+        let check2 = false;
+
+        let loop_breaker = 0;
+
+
+        for (view_distance = 0; view_distance < 10; view_distance++) {
+            opacity_value = 1 - (view_distance / 11);
+            while (to_be_checked_positions.length > 0 ) {
+                current_position = to_be_checked_positions.pop();
+                if(arrays_contains_pos(wall_positions, current_position)){
+                    wall_array[array_index_pos(wall_positions, current_position)].style.opacity = opacity_value;
+                }
+                else {
+                    for (dx = -1; dx < 2; dx++) {
+                        for (dy = -1; dy < 2; dy++) {
+                            if(dx===0 && dy===0){continue;}
+                            next_position = [current_position[0] + dx, current_position[1] + dy];
+                            if (!arrays_contains_pos(checked_positions, next_position)) {
+                                if (!arrays_contains_pos(to_be_checked_next, next_position)) {
+                                    to_be_checked_next.push(next_position);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                checked_positions.push(current_position)
+            }
+            to_be_checked_next.forEach(position_next => {
+                to_be_checked_positions.push(position_next);
+            });
+            to_be_checked_next = [];
 
         }
+
+
     }
 }
